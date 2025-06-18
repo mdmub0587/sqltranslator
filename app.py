f06583f22dfb488296eb36ddeb3a08f9d144d219
Md Mubasir <54818531+mdmub0587@users.noreply.github.com>
2025-06-18 18:47:53 +0000

    edit app code

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from agents import agent
 st.set_page_config(page_title="SQL Translator", layout="wide")
 agent_object = agent()
 
-st.title("🛠️ SQL Translator")
+st.markdown("""
+    <h1 style='text-align: center; color: #4A4A4A;'>🛠️ SQL Translator</h1>
+""", unsafe_allow_html=True)
+st.markdown("###")
 
 # Create two columns for source and target
 col1, col2 = st.columns(2)
